e74bd8e4d339c31f53ee1a5671a20f15ddfd4946
Kate Tyshchenko <54598537+ktyshchenko@users.noreply.github.com>
2021-03-12 17:43:29 -0800

    Add space pod anim control

---
+++ Scripts/PodController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PodController : MonoBehaviour
+{
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameManager.isFinished)
+        {
+            anim.SetBool("isFinished", GameManager.isFinished);
+        }
+    }
+}
