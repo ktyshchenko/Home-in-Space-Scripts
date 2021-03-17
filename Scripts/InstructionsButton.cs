a0f44116ee3d3c859d55c99f0ceed3c0f8b2d091
Kate Tyshchenko <54598537+ktyshchenko@users.noreply.github.com>
2021-03-16 17:02:28 -0700

    Instr button look

---
+++ Scripts/InstructionsButton.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InstructionsButton : ModeButton
+{
+    private Image buttonImg;
+
+    private void Start()
+    {
+        buttonImg = GetComponent<Image>();
+    }
+
+    private void Update()
+    {
+        ActivateButton();
+    }
+
+    private void ActivateButton()
+    {
+        if (Player.instance.activeMode == InputMode.INSTR)
+        {
+            buttonImg.enabled = true;
+        }
+        else
+        {
+            buttonImg.enabled = false;
+        }
+    }
+}
